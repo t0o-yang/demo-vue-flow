e4de1f70833ba538d228bdeebb9f6d255ae23446
yangjh <yangjih0ng@163.com>
2024-09-05 17:08:37 +0800

    需要使用二叉树计算位置

--- src/components/Flow.vue
+++ src/components/Flow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 import type { Node, Edge } from '@vue-flow/core'
 import { Position, VueFlow } from '@vue-flow/core'
 import EndNode from './EndNode.vue'
@@ -297,16 +297,22 @@ const flowData = ref({})
 if (res.sourceSystemArray && res.sceneArray && res.targetSystemArray && res.linksArray) {
   let obj = {
     nodes: [
-      res.sourceSystemArray.map((node, i) => ({
-        id: `0-${i}`,
+      res.sourceSystemArray.map((node, j) => ({
+        id: `0-${j}`,
+        levelI: 0,
+        levelJ: j,
         ...node
       })),
-      res.sceneArray.map((node, i) => ({
-        id: `1-${i}`,
+      res.sceneArray.map((node, j) => ({
+        id: `1-${j}`,
+        levelI: 1,
+        levelJ: j,
         ...node
       })),
-      res.targetSystemArray.map((node, i) => ({
-        id: `2-${i}`,
+      res.targetSystemArray.map((node, j) => ({
+        id: `2-${j}`,
+        levelI: 2,
+        levelJ: j,
         ...node
       }))
     ], // nodes 二维数组，[i][j], i 表示所在的层级
@@ -319,23 +325,29 @@ const rawData = JSON.parse(JSON.stringify(flowData.value))
 const NodeSize = { width: 152, height: 42 }
 console.log('🚀 ~ rawData:===》\n', rawData)
 
-// these are our nodes
-const nodes = ref<Node[]>()
+const nodes = ref<Node[]>([])
+// 包含连线信息的节点
+interface NodeWithRelate {
+  [id: string]: {
+    pre: string[]
+    next: string[]
+  }
+}
+const nodeWithRelate: NodeWithRelate = {}
 
 const padding = {
   left: 10,
   right: 10
 }
 
-const columnItems = reactive({
+const columnItems: {
+  [id: string]: number
+} = reactive({
   '2-0': 3
 })
 
-console.log('🚀 ~ nodes.value=flowData.value.nodes.flatMap ~ nodes.value:===》\n', nodes.value)
-
 // these are our edges
-const edges = ref<Edge[]>()
-console.log('🚀 ~ edges:===》\n', edges.value)
+const edges = ref<Edge[]>([])
 
 function setNodes() {
   nodes.value = flowData.value.nodes.flatMap((level, i, array) => {
@@ -373,20 +385,25 @@ function setNodes() {
       const gap = Gap.height * (j + 1) + nodeHeightCount
       nodeHeightCount += NodeSize.height
       const node = nodes[j]
+      if (node.type == 'set') {
+        // console.log('集合节点内容：', node)
+      }
       flowNodes.push({
         id: node.id,
         position: { x: X, y: gap },
         data: { label: node.title, ...node },
         sourcePosition: 'right',
         targetPosition: 'left',
-        ...nodeProps
+        ...nodeProps,
+        type: nodeProps.type || node.type
       })
     }
     return flowNodes
   })
 }
 
-function setEdges() {
+// 初始化边，将索引转变为 node.id
+function initEdges() {
   edges.value = flowData.value.links.map((ele, i) => {
     ele.source = rawData.nodes.flat()[ele.source].id
     ele.target = rawData.nodes.flat()[ele.target].id
@@ -399,16 +416,21 @@ function setEdges() {
 
 function changeLayout(id, val) {
   columnItems[id] = val
-  findRelatedNode(id)
+  if (nodes.value?.find((node) => node.id == `set:${id}`)) {
+    // console.log('节点已经存在', id, val)
+  } else {
+    // findRelatedNode(id)
+    updateRelatedNode(id)
+  }
 }
 
 function findRelatedNode(rootId) {
-  console.log('源数据', JSON.parse(JSON.stringify(flowData.value)))
+  // console.log('源数据', JSON.parse(JSON.stringify(flowData.value)))
   const relatedEdges = edges.value.filter((edge) => edge.target == rootId)
   const relatedNodes = relatedEdges.map((edge) => {
     return nodes.value.find((node) => node.id == edge.source)
   })
-  console.log('🚀 ~ findRelatedNode ~ relatedNodes:===》\n', relatedEdges, relatedNodes)
+  // console.log('🚀 ~ findRelatedNode ~ relatedNodes:===》\n', relatedEdges, relatedNodes)
   // relatedEdges.forEach((edge) => {
   //   if (edge != null) {
   //     const { source, target } = edge
@@ -428,23 +450,145 @@ function findRelatedNode(rootId) {
       j != -1 && flowData.value.nodes[i].splice(j, 1)
     }
   })
-  console.log('源数据', flowData.value)
+  // console.log('源数据', flowData.value)
 
   const preLevel = rootId.split('-')[0] - 1
   if (preLevel < 0) return
-  // flowData.value.nodes[preLevel].push({
-  //   // id: 'set' + rootId,
-  //   code: '集合',
-  //   title: '集合',
-  //   type: 'setNode'
-  // })
+  flowData.value.nodes[preLevel].push({
+    id: 'set:' + rootId,
+    code: '集合',
+    title: '集合',
+    type: 'set',
+    children: relatedNodes
+  })
+  const addEdges = getEdgesByNodes(relatedNodes, edges.value, 'set:' + rootId)
+  console.log('🚀 ~ findRelatedNode ~ addEdges:===》\n', addEdges)
   setNodes()
-  // setEdges()
+  nextTick(() => {
+    edges.value?.push(...addEdges)
+  })
 }
 
+// 更新节点位置
+function updateNodePosition(nodes: Node[]) {
+  console.log('🚀 ~ updateNodePosition ~ nodes:===》\n', nodes)
+  const levelMap: number[][] = []
+  for (let index = 0; index < nodes.length; index++) {
+    const node = nodes[index]
+    const { levelI } = node.data
+    if (levelMap[levelI] == null) levelMap[levelI] = []
+    levelMap[levelI].push(index)
+  }
+  console.log('🚀 ~ updateNodePosition ~ levelMap:===》\n', levelMap)
+  let levelStartY = {} // 每一层起始 y 坐标
+  let levelMaxWidth: { [level: string]: number } = {} // 每一层的最宽节点
+  for (let index = 0; index < levelMap.length; index++) {
+    const oneLevel = levelMap[index] // 某一层
+    const count = oneLevel.length // 当前层的节点数量
+    let oneLevelHeight = 0 // 当前层节点总高度
+    let oneLevelMaxWidth = 0 // 当前层的最宽节点的宽度
+    for (let index = 0; index < oneLevel.length; index++) {
+      const nodei = oneLevel[index]
+      oneLevelHeight += nodes[nodei].data.size?.height || NodeSize.height
+      const nodeWidth = nodes[nodei].data.size?.width || NodeSize.width
+      if (oneLevelMaxWidth < nodeWidth) {
+        oneLevelMaxWidth = nodeWidth
+      }
+    }
+    levelMaxWidth[index] = oneLevelMaxWidth
+  }
+
+  // TODO 获取某一层的起始 y 位置 , 当节点更窄时，需要进一步计算位置
+}
+
+// 更新相关节点的关系
+function updateRelatedNode(rootId: string) {
+  const preLevel = Number(rootId.split('-')[0]) - 1
+  if (preLevel < 0) return
+  const setId = 'set:' + rootId
+  // 添加一个集合节点
+  nodes.value.push({
+    id: setId,
+    position: { x: 0, y: 0 },
+    data: {
+      levelI: preLevel,
+      size: {
+        width: NodeSize.width * columnItems[rootId],
+        height: NodeSize.height * columnItems[rootId]
+      }
+    },
+    type: 'set'
+  })
+
+  // 设置关联节点的父级节点 = 新的集合节点
+  const relatedEdges = edges.value.filter((edge) => edge.target == rootId)
+  const relatedNodes = relatedEdges.map((edge) => {
+    return nodes.value.find((node) => node.id == edge.source)
+  })
+  for (let index = 0; index < relatedEdges.length; index++) {
+    const edge = relatedEdges[index]
+    nodes.value?.forEach((node) => {
+      if (node.id == edge.source) {
+        node.parentNode = setId
+      }
+    })
+  }
+
+  updateNodePosition(nodes.value)
+
+  const addEdges = getEdgesByNodes(relatedNodes, edges.value, setId)
+  nextTick(() => {
+    edges.value?.push(...addEdges)
+  })
+}
+
+// 获取节点关联的所有边
+function getEdgesByNodes(nodes: any[], edges: Edge[], setId: string) {
+  const replaceEdges: Edge[] = [] // 新节点，需要补充的边
+  nodes.forEach((node) => {
+    for (let index = 0; index < edges.length; index++) {
+      const edge = edges[index]
+      let newEdge: Edge | undefined = undefined
+      if (edge.source == node.id) {
+        newEdge = {
+          id: `${setId}->${edge.target}`,
+          source: setId,
+          target: edge.target,
+          animated: true
+        }
+        if (nodeWithRelate[node.id]?.next == null) {
+          nodeWithRelate[node.id]['next'] = []
+        }
+        if (!nodeWithRelate[node.id]['next'].includes(edge.target))
+          nodeWithRelate[node.id]['next'].push(edge.target)
+      } else if (edge.target == node.id) {
+        newEdge = {
+          id: `${edge.source}->${setId}`,
+          source: edge.source,
+          target: setId,
+          animated: true
+        }
+        if (nodeWithRelate[node.id]?.pre == null)
+          nodeWithRelate[node.id] = {
+            pre: []
+          }
+        if (!nodeWithRelate[node.id]['pre'].includes(edge.source))
+          nodeWithRelate[node.id]['pre'].push(edge.source)
+      }
+      if (newEdge) {
+        replaceEdges.find((edge) => edge.id == newEdge!.id) == null && replaceEdges.push(newEdge)
+      }
+    }
+  })
+  return replaceEdges
+}
+
+function hover(node: Node) {
+  nodes.value?.push(node)
+}
 onMounted(() => {
   setNodes()
-  setEdges()
+  initEdges()
 })
 </script>
 
@@ -460,7 +604,12 @@ onMounted(() => {
       />
     </template>
     <template #node-set="props">
-      <SetNode :id="props.id" :data="props.data" />
+      <SetNode
+        :id="props.id"
+        :data="props.data"
+        :col-number="columnItems[props.id.split(':')[1]]"
+        @hover="hover"
+      />
     </template>
 
     <!-- bind your custom edge type to a component by using slots, slot names are always `edge-<type>` -->
--- src/components/SetNode.vue
+++ src/components/SetNode.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="set-node"></div>
+  <div
+    class="set-node"
+    :style="{
+      width: `${152 * colNumber}px`,
+      height: `${42 * colNumber}px`,
+      overflowY: 'auto',
+      gridTemplateColumns: '1fr '.repeat(colNumber)
+    }"
+  >
+    <div
+      class="set-node-childrenNode"
+      v-for="node in data.children"
+      @mouseover="(e) => mouseover(node, e)"
+    >
+      {{ node.data.label }}
+    </div>
+  </div>
   <Handle id="a" type="target" :position="Position.Left" />
   <Handle id="b" type="source" :position="Position.Right" />
 </template>
@@ -7,6 +23,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { Handle, Position, useVueFlow } from '@vue-flow/core'
+import type { Node } from '@vue-flow/core'
 
 const props = defineProps({
   id: {
@@ -16,12 +33,40 @@ const props = defineProps({
   data: {
     type: Object,
     required: true
+  },
+  colNumber: {
+    type: Number
   }
 })
+const emits = defineEmits(['hover'])
+console.log('🚀 ~ props:===》\n', props.id, props.colNumber)
+
+function mouseover(node: Node, e) {
+  node.position = {
+    x: e.x - 354,
+    y: e.y - 80
+  }
+  console.log('🚀 ~ mouseover ~ node:===》\n', node)
+  console.log('🚀 ~ mouseover ~ e:===》\n', e)
+}
 </script>
 
 <style scoped>
 .set-node {
-  border: 1px dotted;
+  padding: 5px;
+  border: 1px dashed black;
+  border-spacing: 15px;
+  border-radius: 3px;
+  display: grid;
+  grid-gap: 5px 5px;
+}
+.set-node-childrenNode {
+  /* width: 152px; */
+  /* height: 21px; */
+  border: 1px solid;
+  border-radius: 3px;
+}
+.set-node-childrenNode:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 </style>
